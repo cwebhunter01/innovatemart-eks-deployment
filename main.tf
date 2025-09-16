8242c1f8b420fda03577b91053d824eb324c9180
Adeyemi <favourolukokun@gmail.com>
2025-09-16 21:29:07 +0100

    Fix: use migrate-state for Terraform backend

--- main.tf
+++ main.tf
@@ -1,4 +1,15 @@
 terraform {
+  backend "s3" {
+    bucket         = "innovatemart-tfstate-n38gnu"
+    key            = "terraform/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
+
+
+terraform {
   required_version = ">= 1.5.0"
 
   required_providers {
